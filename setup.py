539e1aa09fb3d89d463be22172e72576a040cd1e
Derek Brans <dbrans@gmail.com>
2013-01-07 16:05:26 -0800

    pip packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name = 'useragentutils',
+  packages = ['useragentutils'],
+  version = '0.1.1',
+  description = "Python port of the Java user-agent-utils lib",
+  author='Derek Brans',
+  author_email='dbrans@gmail.com',
+  url='https://github.com/krux/python-user-agent-utils',
+  classifiers=[]
+)
