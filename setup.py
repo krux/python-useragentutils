0a72e91e6268161471c0cac490bbcfcbf5e93c34
jnewman <joshua.newman@krux.com>
2015-02-06 13:15:41 -0800

    Basic cleanup  - Switch to current code standards  - Remove unnecessary imports  - Switch quotes  - Cleanup newlines  - Use nose  - Add code coverage  - Add testing instructions  - Add info about deployments

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 setup(
-  name = 'useragentutils',
-  packages = ['useragentutils'],
-  version = '0.1.1',
-  description = "Python port of the Java user-agent-utils lib",
-  author='Derek Brans',
-  author_email='dbrans@gmail.com',
-  url='https://github.com/krux/python-useragentutils',
-  classifiers=[]
+    name='useragentutils',
+    packages=['useragentutils'],
+    version='0.1.1',
+    description='Python port of the Java user-agent-utils lib',
+    author='Derek Brans',
+    author_email='dbrans@gmail.com',
+    maintainer='Joshua Newman',
+    maintainer_email='josh@krux.com',
+    url='https://github.com/krux/python-useragentutils',
+    classifiers=[]
 )
--- useragentutils/version.py
+++ useragentutils/version.py
@@ -1,16 +1,16 @@
 import utilities as util
 
+
 class Version(util.Base):
-  __metaclass__ = type
+    __metaclass__ = type
 
-  def __init__(self, version, majorVersion, minorVersion):
-    self.version = version
-    self.majorVersion = majorVersion
-    self.minorVersion = minorVersion
+    def __init__(self, version, majorVersion, minorVersion):
+        self.version = version
+        self.majorVersion = majorVersion
+        self.minorVersion = minorVersion
 
-  def __str__(self):
-    return self.version
+    def __str__(self):
+        return self.version
 
-  def __repr__(self):
-    return "<"+ type(self).__name__ + ":" + self.majorVersion + "." + self.minorVersion + ">"
-
+    def __repr__(self):
+        return '<' + type(self).__name__ + ':' + self.majorVersion + '.' + self.minorVersion + '>'
