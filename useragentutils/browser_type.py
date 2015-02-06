1c60da31ef2617497b6e41e721bbef1a4a7f78c9
jnewman <joshua.newman@krux.com>
2015-02-06 13:15:41 -0800

    Basic cleanup  - Switch to current code standards  - Remove unnecessary imports  - Switch quotes  - Cleanup newlines  - Use nose  - Add code coverage  - Add testing instructions  - Add info about deployments  - cover correct package  - Rev version

--- useragentutils/browser_type.py
+++ useragentutils/browser_type.py
@@ -1,38 +1,38 @@
 from utilities import Enum, EnumValue
 
+
 class BrowserType(Enum):
+    def __init__(self, name):
+        self.name = name
 
-  def __init__(self, name):
-    self.name = name
+    ##
+    # Standard web-browser
+    ##
+    WEB_BROWSER = EnumValue('Browser')
 
-  ##
-  # Standard web-browser
-  ##
-  WEB_BROWSER = EnumValue("Browser")
+    ##
+    # Special web-browser for mobile devices
+    ##
+    MOBILE_BROWSER = EnumValue('Browser (mobile)')
 
-  ##
-  # Special web-browser for mobile devices
-  ##
-  MOBILE_BROWSER = EnumValue("Browser (mobile)")
+    ##
+    # Text only browser like the good old Lynx
+    ##
+    TEXT_BROWSER = EnumValue('Browser (text only)')
 
-  ##
-  # Text only browser like the good old Lynx
-  ##
-  TEXT_BROWSER = EnumValue("Browser (text only)")
+    ##
+    # Email client like Thunderbird
+    ##
+    EMAIL_CLIENT = EnumValue('Email Client')
 
-  ##
-  # Email client like Thunderbird
-  ##
-  EMAIL_CLIENT = EnumValue("Email Client")
+    ##
+    # Search robot, spider, crawler,...
+    ##
+    ROBOT = EnumValue('Robot')
 
-  ##
-  # Search robot, spider, crawler,...
-  ##
-  ROBOT = EnumValue("Robot")
+    ##
+    # Downloading tools
+    ##
+    TOOL = EnumValue('Downloading tool')
 
-  ##
-  # Downloading tools
-  ##
-  TOOL = EnumValue("Downloading tool")
-
-  UNKNOWN = EnumValue("unknown")
+    UNKNOWN = EnumValue('unknown')
